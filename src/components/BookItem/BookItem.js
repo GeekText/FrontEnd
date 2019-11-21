a7b255c7ccde055ba72094e52fd43b3ab5a4a1a2
EstebanH <ehuez002@fiu.edu>
2019-11-21 18:04:03 -0500

    Test for Tim

--- src/components/BookItem/BookItem.js
+++ src/components/BookItem/BookItem.js
@@ -6,9 +6,20 @@ import { addItemWish } from "../wishlist/WishlistFunctions";
 import { Link } from "react-router-dom";
 import { filtered } from "../Filter/FilterFunctions";
 import "./BookItem.css";
+import Popup from "./BookPop";
 
 export class Bookitem extends Component {
-  // style= {"width": "18rem"}
+  constructor(props) {
+    super(props);
+    this.state = { showPopup: false };
+  }
+
+  togglePopup() {
+    console.log("Clicked");
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
 
   clickOn = id => {
     this.props.addItem(id);
@@ -51,7 +62,16 @@ export class Bookitem extends Component {
               alt="bookcover placeholder"
               width="200"
               height="200"
+              onClick={() => {
+                this.togglePopup();
+              }}
             ></img>
+            {this.state.showPopup ? (
+              <Popup
+                book_cover={this.props.book.book_cover}
+                closePopup={this.togglePopup.bind(this)}
+              />
+            ) : null}
           </div>
           <div className="details">
             <span className="book_title">{this.props.book.book_name}</span>
@@ -98,7 +118,6 @@ export class Bookitem extends Component {
                 {" "}
                 Published by {this.props.book.book_publisher}
               </span>
-              
             </div>
           </div>
         </div>
---
+++ src/components/BookItem/BookPop.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class Popup extends React.Component {
+  render() {
+    return (
+      <div className="popup">
+        <div className="popup_inner">
+          <img
+            src={this.props.book_cover}
+            alt="Failed to load: book_cover"
+            width="600"
+            height="600"
+            className="image"
+          />
+          <div className="container"></div>
+          <button className="popup-button" onClick={this.props.closePopup}>
+            Close
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Popup;
