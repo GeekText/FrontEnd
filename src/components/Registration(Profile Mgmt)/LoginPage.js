8fca187165971ff5bb2ebbb9d2c3b0c57c39706b
EstebanH <ehuez002@fiu.edu>
2019-11-22 16:19:35 -0500

    Added checkout Removed img bug Added Paul's part

--- src/components/Registration(Profile Mgmt)/LoginPage.js
+++ src/components/Registration(Profile Mgmt)/LoginPage.js
@@ -1,25 +1,49 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./style.css";
 class Login extends React.Component {
+  handleSubmit = () => {
+    // method to handle submission of Login and Password
+  };
   render() {
     return (
-      <div className="container">
-        <div className="form-header">
-          <h4 className="title">Welcome to the Account Login Page</h4>
+      <div className="base-container">
+        <h1>Login Page</h1>
+        <p>Enter your login credentials in the fields below</p>
+        <div className="form">
+          <form onSubmit={this.handleSubmit}>
+            <div>
+              <label htmlFor="username">Login ID</label>
+              <input
+                type="text"
+                placeholder="Login ID"
+                name="loginID"
+                required={true}
+              />{" "}
+              <hr />
+            </div>
+            <div>
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                required={true}
+              />
+            </div>
+            <br /> <br />
+            <div className="submit_btn" style={{ textAlign: "center" }}>
+              <input type="submit" value="Submit" name="login"></input>
+            </div>
+            <br />
+          </form>
         </div>
-        Enter your existing login credentials in the fields below to proceed to
-        proceed: <br /> <br />
-        <form>
-          Login ID: <input type="text" id="LoginID" required /> <br />
-          Password: <input type="text" id="Password" required /> <br />
-          <input className="submit-button" type="submit" value="Submit"></input>
-        </form>
         <Link to="/profile">
-          <span href="#create" className="account-link" type="button">
-            Don't have an account? Click here to create one.
+          <span href="#create" className="account-link">
+            Not a returning user? Click here to create an account.
           </span>
         </Link>
+        <br />
       </div>
     );
   }
